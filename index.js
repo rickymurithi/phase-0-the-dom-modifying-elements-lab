93a378f3b53b6ba342ad9f392bb721b336538cc6
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-28 12:42:28 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+//code!
+document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
